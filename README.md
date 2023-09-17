892193201196e08ddc2f78d4421b402919ce69a0
Puneet Bajaj <bajajpuneet223@gmail.com>
2023-09-17 15:17:01 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# NodePostManager - RESTful Post Management
+
+**NodePostManager --Quora**  is a RESTful web application for efficient post management. It's designed to simplify the process of adding, deleting, searching, and editing posts following REST principles. Built with Node.js and Express.js, it leverages key libraries like Method Override and UUID for a seamless user experience.
+
+## Features
+
+- **Add Posts:** Easily create and publish new posts using RESTful POST requests.
+- **Delete Posts:** Remove posts you no longer need with RESTful DELETE requests.
+- **Search Posts:** Quickly find specific posts using a RESTful GET request.
+- **Edit Posts:** Update and modify existing posts using RESTful PUT requests.
+
+## Technologies Used
+
+- Node.js
+- Express.js
+- Method Override
+- UUID
+
+## Installation
+
+1. Clone this repository to your local machine.
+2. Run `npm install` to install the required dependencies.
+3. Start the application with `npm index.js`.
+4. Open your web browser and visit `http://localhost:7860` to access NodePostManager.
+
+## Usage
+
+1. Add new posts by making a RESTful POST request.
+2. Delete posts using a RESTful DELETE request.
+3. Search for posts using a RESTful GET request.
+4. Edit posts by making a RESTful PUT request and specifying the post to update.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests to help improve this project.
+
+
+## Author
+
+**Puneet Bajaj** from **Indian Institute of Technology , Madars**
+
+## Acknowledgments
+
+- Thanks to the Node.js and Express.js communities for their excellent documentation and libraries.
---
+++ index.js
@@ -0,0 +1,94 @@
+const { log } = require("console");
+const exp = require("constants");
+const express = require("express");
+const path = require("path");
+const app = express();
+const {v4: uuidv4} = require("uuid");
+const port = 7860;
+const methodOverride = require("method-override");
+
+app.use(express.urlencoded({extended : true}));
+
+app.use(methodOverride("_method"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        id: uuidv4(),
+        username: "Gaangu..",
+        content: "I love you my baby, you are my soulmate"
+    },
+    {
+        id: uuidv4(),
+        username: "Lola..",
+        content: "hello apan hai lolla"
+    },
+    {
+        id: uuidv4(),
+        username: "Pagle",
+        content: "I am joi jdnwidn knxi "
+    }
+];
+
+app.get("/posts", (req, res)=>{
+    res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res)=>{
+    res.render("newpost.ejs");
+})
+
+app.post("/posts", (req, res)=>{
+    let {username, content} = req.body;
+
+    posts.push({id : uuidv4(), username, content});
+    
+    res.redirect("/posts");
+})
+
+app.post("/posts/search", (req, res)=>{
+    let {id} = req.body;
+    res.redirect(`/posts/search/${id}`);
+})
+
+app.get("/posts/search/:id", (req, res) => {
+    let {id} = req.params;
+    let postsByUser = [];
+    for (const post of posts) {
+        if(post.id == id || post.username == id ){
+            postsByUser.push(post);
+        }
+    }
+    res.render("idposts.ejs", {postsByUser});
+})
+
+app.get("/posts/update/:id", (req, res) =>{
+    let {id} = req.params;    
+    let post = posts.find((p) => id === p.id);
+
+    res.render("updatePost", {id, username: post.username, content: post.content});
+
+})
+
+app.patch("/posts/update/:id", (req, res) =>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id == p.id);
+
+    post.content = newContent;
+    
+    res.redirect("/posts");
+   
+})
+
+app.delete("/posts/delete/:id", (req, res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+
+    res.redirect("/posts");
+})
+
+app.listen(port, ()=>{console.log("listening on port", port)});
